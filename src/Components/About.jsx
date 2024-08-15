8816ab48dc7020f3242ff4bb89ec66d0e3e25c5e
Shahzal Rehman <119114795+Shahzal-Rehman@users.noreply.github.com>
2024-08-15 18:18:53 +0500

    Update About.jsx
    Signed-off-by: Shahzal Rehman <119114795+Shahzal-Rehman@users.noreply.github.com>

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Microsoft Official Title Holder and currently studynig at Punjab Group of Colleges, Hasilpur Campus for HSSC ICS_Phy Session 2023 to 2025 after Stdying At Fauji Foundation School , Hasilpur Cmpus for SSC Bio Session 2021 to 2023. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
